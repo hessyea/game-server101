32d2ee590f35e2f632bb54968bbf9d8606433201
hessyea <32811265+hessyea@users.noreply.github.com>
2019-04-10 22:47:38 +0200

    Update app.js
    force

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ var app = express();
 const https = require('https');
 app.set('port', process.env.PORT || 3000);
 
+var forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+ };
+
+
 app.configure(function(){
 	app.use(express.methodOverride());
 	app.use(express.bodyParser());
 	app.use(app.router);
+	app.use(forceSsl);
 	app.set('view engine', 'jade');
 	app.set('views', __dirname + '/public');
 	app.set('view options', {layout: false});
@@ -17,18 +26,12 @@ app.configure(function(){
 
 
 
-
 app.configure('production', function(){
 	var oneYear = 31557600000;
 	app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
 	app.use(express.errorHandler());
-	app.use(function (req, res, next) {
-    const secureUrl = 'https://' + req.hostname + req.originalUrl
-    res.redirect(302, secureUrl)
+});
 
-  next()
-});
-});
 
 console.log("Web server has started.\nPlease log on http://127.0.0.1:3002/index.html");
 app.listen(app.get('port'));
