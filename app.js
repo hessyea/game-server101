7e96cb58d2d267717cb8fcff4ac64b54de0ab08f
hessyea <32811265+hessyea@users.noreply.github.com>
2019-04-10 22:53:41 +0200

    Update app.js
    noif

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const https = require('https');
 app.set('port', process.env.PORT || 3000);
 
 var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
+    
         return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
+    
     return next();
  };
 
