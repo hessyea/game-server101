c3b6b674f1dd141719635c6dc1bf3f0bd8aab4dd
hessyea <32811265+hessyea@users.noreply.github.com>
2019-04-10 23:40:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.configure(function(){
 
 
 app.configure('production', function(){
-	app.use(forceSsl);
 	var oneYear = 31557600000;
 	app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
 	app.use(express.errorHandler());
