5c89e129550263c6578da12a34a548963d573f5b
hessyea <32811265+hessyea@users.noreply.github.com>
2019-04-10 23:42:47 +0200

    Update app.js
    again

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
-var forceSsl = function (req, res, next) {
-    
+ var forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
         return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    
+    }
     return next();
  };
+
 
 
 app.configure(function(){
